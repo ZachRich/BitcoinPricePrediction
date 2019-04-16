9e7a9ecf9c5d27026b1f53ad1f0f75573ac9f4bf
Zachary Rich <zachrichdev@gmail.com>
2019-04-16 09:58:31 -0400

    Linear Regression kind of implemented

--- SemesterProject.py
+++ SemesterProject.py
@@ -1,69 +1,63 @@
+# Bitcoin Price prediction with Polynomial Regression
 
-
-import csv
-import matplotlib.pyplot as plt
-
-# Importing Data
+# Imports
+from matplotlib import pyplot as plt
+import pandas as pd
+from sklearn import linear_model
+from sklearn.model_selection import train_test_split
 
 path = '/Users/zacharyrich/Desktop/bitcoin_cash_price.csv'
 
-X = []
-y = []
+dataset = pd.read_csv(path)
 
-with open(path) as csv_file:  # Reading in data from CSV
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-        tempRow = row[0].split(",")  # Splitting data by ","
-        DOW = tempRow[0]
+# Replace Days of week with their Numbers
+replacements = {
+    'Sunday': 0,
+    'Monday': 1,
+    'Tuesday': 2,
+    'Wednesday': 3,
+    'Thursday': 4,
+    'Friday': 5,
+    'Saturday': 6
+}
 
-        if DOW == 'Sunday':  # 0-6 for days of the week starting at Sunday
-            DOW = 0
-        elif DOW == 'Monday':
-            DOW = 1
-        elif DOW == 'Tuesday':
-            DOW = 2
-        elif DOW == 'Wednesday':
-            DOW = 3
-        elif DOW == 'Thursday':
-            DOW = 4
-        elif DOW == 'Friday':
-            DOW = 5
-        elif DOW == 'Saturday':
-            DOW = 6
+dataset.replace(replacements, inplace=True, )
 
-        row[0] = DOW  # Replacing dates with Quantifiers
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 3:4].values
 
-        X.append([row[0], row[1], row[2], row[3]])
-        y.append(row[4])
+# create training and testing vars
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+print(X_train.shape, y_train.shape)
+print(X_test.shape, y_test.shape)
 
 # Fitting Linear Regression to the dataset
 from sklearn.linear_model import LinearRegression
-
 lin_reg = LinearRegression()
-lin_reg.fit(X, y)
+lin_reg.fit(X_train, y_train)
+
 
 # Fitting Polynomial Regression to the dataset
 from sklearn.preprocessing import PolynomialFeatures
 
 poly_reg = PolynomialFeatures(degree=4)
-X_poly = poly_reg.fit_transform(X)
+X_poly = poly_reg.fit_transform(X_train)
 
 lin_reg2 = LinearRegression()
-lin_reg2.fit(X_poly, y)
+lin_reg2.fit(X_poly, y_train)
 
-# Visualizaing the Linear Regression Results
-plt.scatter(X, y, color='red')  # Plots the actual data
+# # Visualizaing the Linear Regression Results
+plt.scatter(y_test, lin_reg.predict(X_test), color='red')  # Plots the actual data
 plt.plot(X, lin_reg.predict(X), color='blue')  # Plots the predicted data
-plt.title('Truth or Bluff (Linear Regresion)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.title('Bitcoin Price Prediction(Linear Regresion)')
+plt.xlabel('Features(Day of week, High, Low, Open)')
+plt.ylabel('Close Price')
 plt.show()
 
 # Visualizing the Polynomial Regression Results
-plt.scatter(X, y, color='red')  # Plots the actual data
+plt.scatter(y_test, lin_reg2.predict(X_test), color='red')  # Plots the actual data
 plt.plot(X, lin_reg2.predict(X_poly), color='blue')  # Predicted data
-plt.title('Truth or Bluff(Polynomial Regression, degree 4)')
-plt.xlabel('Posiiton Level')
-plt.ylabel('Salary')
+plt.title('Bitcoin Price Prediction(Polynomial Regresion)')
+plt.xlabel('Features(Day of week, High, Low, Open)')
+plt.ylabel('Close Price')
 plt.show()
