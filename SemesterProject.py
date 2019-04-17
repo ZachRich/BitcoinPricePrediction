d9a24acdee51218cf14ab9ff7741fb0441dbe3bf
Zachary Rich <ZachRichDev@gmail.com>
2019-04-17 16:36:43 -0400

    We kinda did a thing, maybe?

--- SemesterProject.py
+++ SemesterProject.py
@@ -5,8 +5,13 @@ from matplotlib import pyplot as plt
 import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.svm import SVR
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import accuracy_score
 
-path = '/Users/zacharyrich/Desktop/bitcoin_cash_price.csv'
+path = '/Users/zacharyrich/Desktop/bitcoin_price.csv'
 
 dataset = pd.read_csv(path)
 
@@ -23,18 +28,31 @@ replacements = {
 
 dataset.replace(replacements, inplace=True, )
 
+plt.scatter(dataset.iloc[:, 0:1].values, dataset.iloc[:, 3:4].values, color='green')  # Plots the actual data
+plt.title('Day Of the Week + Close Price')
+plt.xlabel('Day of the week')
+plt.ylabel('Close Price')
+plt.show()
+
+plt.scatter(dataset.iloc[:, 1:2].values, dataset.iloc[:, 3:4].values, color='red')  # Plots the actual data
+plt.title('High + Close Price')
+plt.xlabel('High Price')
+plt.ylabel('Close Price')
+plt.show()
+
+plt.scatter(dataset.iloc[:, 2:3].values, dataset.iloc[:, 3:4].values, color='blue')  # Plots the actual data
+plt.title('Low + Close Price')
+plt.xlabel('Low Price')
+plt.ylabel('Close Price')
+plt.show()
+
+
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3:4].values
 
-# create training and testing vars
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
-print(X_train.shape, y_train.shape)
-print(X_test.shape, y_test.shape)
+from sklearn.model_selection import train_test_split
 
-# Fitting Linear Regression to the dataset
-from sklearn.linear_model import LinearRegression
-lin_reg = LinearRegression()
-lin_reg.fit(X_train, y_train)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 
 # Fitting Polynomial Regression to the dataset
@@ -46,18 +64,7 @@ X_poly = poly_reg.fit_transform(X_train)
 lin_reg2 = LinearRegression()
 lin_reg2.fit(X_poly, y_train)
 
-# # Visualizaing the Linear Regression Results
-plt.scatter(y_test, lin_reg.predict(X_test), color='red')  # Plots the actual data
-plt.plot(X, lin_reg.predict(X), color='blue')  # Plots the predicted data
-plt.title('Bitcoin Price Prediction(Linear Regresion)')
-plt.xlabel('Features(Day of week, High, Low, Open)')
-plt.ylabel('Close Price')
-plt.show()
+y_pred = lin_reg2.predict(X_poly)
 
-# Visualizing the Polynomial Regression Results
-plt.scatter(y_test, lin_reg2.predict(X_test), color='red')  # Plots the actual data
-plt.plot(X, lin_reg2.predict(X_poly), color='blue')  # Predicted data
-plt.title('Bitcoin Price Prediction(Polynomial Regresion)')
-plt.xlabel('Features(Day of week, High, Low, Open)')
-plt.ylabel('Close Price')
-plt.show()
+confidence = lin_reg2.score(X_test, y_test)
+print("confidence: ", confidence)
