9e598a545bbf5149bc0c14d521a3f28679dc11f3
Zachary Rich <32521939+ZachRich@users.noreply.github.com>
2019-05-06 15:25:21 -0400

    Update Regression.py
    Fixed problem with weights in regression

--- Regression.py
+++ Regression.py
@@ -3,6 +3,7 @@
 # Importing the libraries
 import numpy as np
 import pandas as pd
+from pandas import DataFrame
 from sklearn.linear_model import LinearRegression
 
 # Importing the dataset
@@ -17,11 +18,20 @@ y_low = dataset.iloc[:, 3:4].values  # Low Prices
 
 y_close = dataset.iloc[:, -1].values  # Close Prices
 
+
+data = {'Open': X_Open, 'High': y_High, 'Low': y_low, 'Close': y_close}
+df = DataFrame(dataset,columns=['Open','High','Low','Close'])
+
+X = df[['High', 'Low', 'Close']]
+y = df['Open']
+
+reg = LinearRegression()
+reg.fit(X, y)
+
 def linear_prediction(open_price):
-    testClosePrice = 5140.38
+    testClosePrice = 6524.17
+
     # Fitting Linear Regression to the dataset
-    lin_reg_open = LinearRegression()
-    lin_reg_open.fit(X_Open, y_close)
 
     lin_reg_high = LinearRegression()
     lin_reg_high.fit(X_Open, y_High)
@@ -30,16 +40,16 @@ def linear_prediction(open_price):
     lin_reg_low.fit(X_Open, y_low)
 
     # Linear Predictions
+
     x_test_high = np.array(open_price)
-    pred_high = lin_reg_high.predict(x_test_high.reshape(1, -1))  # High
+    pred_high = lin_reg_high.predict(x_test_high.reshape(1, -1))  #Predict High
 
     x_test_low = np.array(open_price)
-    pred_low = lin_reg_low.predict(x_test_low.reshape(1, -1))  # Low
+    pred_low = lin_reg_low.predict(x_test_low.reshape(1, -1))  #Predict Low
 
-    x_test = np.array((open_price + pred_high + pred_low) / 3)  # Open
-    y_pred = lin_reg_open.predict(x_test.reshape(1, -1))
+    prediction = reg.predict([[open_price, pred_high, pred_low]])
 
-    error = (((testClosePrice - y_pred) / testClosePrice) * 100).__abs__()
+    error = (((testClosePrice - prediction) / testClosePrice) * 100).__abs__()
 
     print("Linear Regression")
 
@@ -49,11 +59,10 @@ def linear_prediction(open_price):
 
     print("Predicted Low Price: ", pred_low)
 
-    print("Predicted Close Price: ", y_pred)
+    print("Predicted Close Price: ", prediction)
 
     print("Percent Error: ", error)
 
-    return open_price, pred_high, pred_low, y_pred, error
+    return open_price, pred_high, pred_low, prediction, error
 
-
-linear_prediction(5140.38)
+linear_prediction(6350)
